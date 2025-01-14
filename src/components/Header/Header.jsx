e512cda5305c32ee282d94f70ce47be4e538a0e5
Catalina <cblvallada@gmail.com>
2025-01-14 12:54:52 +0100

    Fix html problems in the Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "antd";
 import { useDispatch, useSelector } from "react-redux";
@@ -25,20 +25,30 @@ const Header = () => {
 
   return (
     <div>
-      <Link to="/">Home</Link> /
-      <input onKeyUp={handleChange} placeholder="Buscar publicación" name="text" />
-      {user ? (
-        <>
-        <Link to="/profile">{user.name}</Link>
-          <Button onClick={onLogout}>Logout</Button>
-        </>
-      ) : (
-        <>
-          <Link to="/login">Login</Link> /<Link to="/register">Register</Link> /
-        </>
-      )}
+      <div className="parent">
+        <div className="left">
+        <Link className="title" to="/">
+              Mern
+            </Link>/
+            <div className="searchbar">
+            <input onKeyUp={handleChange} placeholder="Buscar publicación" name="text" />
+            </div>
+          {user ? (
+            <>
+            <Link to="/profile">{user.name}</Link>
+              <Button onClick={onLogout}>Logout</Button>
+            </>
+          ) : (
+            
+            <div className="right">
+            <Link className="normal-link" to="/login">Login</Link> /<Link to="/register">Register</Link>
+          </div>
+        )}
+          </div>
+          </div>
+        <div className="line"></div>
     </div>
-  );
-};
+  )
+}
 
 export default Header;
