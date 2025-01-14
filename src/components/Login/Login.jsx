0e45e2dac09b505cc4c21f76a5c7c802b9f10d0b
Catalina <cblvallada@gmail.com>
2025-01-14 12:26:21 +0100

    Translate inputs to spanish

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
             <form className="login-form" onSubmit={onSubmit}>
                 <input
                 type="email"
-                placeholder="Email"
+                placeholder="Correo electrónico"
                 name="email"
                 value={email}
                 onChange={onChange}
@@ -38,13 +38,13 @@ const Login = () => {
                 />
                 <input
                 type="password"
-                placeholder="Password"
+                placeholder="Contraseña"
                 name="password"
                 value={password}
                 onChange={onChange}
                 required
                 />
-                <button type="submit">Login</button>
+                <button type="submit">Iniciar sesión</button>
             </form>
             <p>Si ya tienes una cuenta, registrate <Link to="/register">aquí</Link></p>
             </div>
